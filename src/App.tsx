bfa76a6d519b3699fe5a2d4ba855a94f135da2c1
Itay Levy <itaylevy134@gmail.com>
2020-03-13 22:32:11 +0200

    Login is now at /login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import SignIn from './SignIn'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
     return (
-        <SignIn/>
+        <Router>
+            <Route component={SignIn} path='/login'/>
+        </Router>
     );
 }
 
