b9c558e916af883194e89e1bc876d6b7f77060d2
Itay Levy <itaylevy134@gmail.com>
2020-03-18 06:43:57 +0200

    removed old sign up and login pages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import SignIn from './SignIn'
-import SignUp from './SignUp'
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route component={SignIn} path='/login'/>
-        <Route component={SignUp} path='/sign-up'/>
       </Switch>
     </Router>
   );
