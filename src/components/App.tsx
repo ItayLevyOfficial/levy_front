a67f4883d9e1d45015d50d67a3f213fcc83eadaf
Itay Levy <itaylevy134@gmail.com>
2020-03-19 03:58:55 +0200

    created a login with instagram button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function instagramLogin(){
+  console.log('logined');
+}
 
 function App() {
+  const responseInstagram = (response) => {
+    console.log(response);
+  }
   return (
     <Router>
       <Switch>
+        <button onClick={instagramLogin}>
+          login with instagram
+        </button>
       </Switch>
     </Router>
   );
