8086b32adb934562794854938f7e38406e3bcd64
Itay Levy <itaylevy134@gmail.com>
2020-03-19 04:46:02 +0200

    Updates

---
+++ precache-manifest.7adf226552028290a798fdf8a463c592.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ecdff07ad29e4cadc1045be36ae974",
+    "url": "/levy_front/index.html"
+  },
+  {
+    "revision": "84568da79d728cecb7bf",
+    "url": "/levy_front/static/js/2.b4099da3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/levy_front/static/js/2.b4099da3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c95ae7e07777ab6bb06b",
+    "url": "/levy_front/static/js/main.e0e50a02.chunk.js"
+  },
+  {
+    "revision": "5e6559727b0fde7087a2",
+    "url": "/levy_front/static/js/runtime-main.9ad7f1a4.js"
+  }
+]);
