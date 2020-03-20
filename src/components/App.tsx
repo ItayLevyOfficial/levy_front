e3a4387cd5d225ef99bc030b94af006d05016874
Itay Levy <itaylevy134@gmail.com>
2020-03-20 03:14:20 +0200

    Working instagram login button that return a code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import routes from './routes';
+import InstagramLogin from 'react-instagram-login';
 
-function instagramLogin(){
-  console.log('logined');
+const responseInstagram = (response) => {
+  console.log(response);
+}
+
+const Login: React.FC = () => {
+  return (
+    <InstagramLogin
+      clientId="498277677742228"
+      buttonText="Login"
+      onSuccess={responseInstagram}
+      onFailure={responseInstagram}
+      scope="user_profile"
+      redirectUri="https://itaylevyofficial.github.io/levy_front/"
+      style={{justifyContent: 'center'}}
+    />
+  )
 }
 
 function App() {
-  const responseInstagram = (response) => {
-    console.log(response);
-  }
   return (
     <Router>
       <Switch>
-        <button onClick={instagramLogin}>
-          login with instagram
-        </button>
+        <Route
+          path={routes.instagram_login}
+          component={Login}
+        />
       </Switch>
     </Router>
   );
