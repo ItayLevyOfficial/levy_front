79e8b172f7a7b692b8fd97bc94df65572d420c41
Itay Levy <itaylevy134@gmail.com>
2020-03-21 07:47:28 +0200

    refactor: extracted instagram login into a new file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,36 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React from "react";
 import routes from "./routes";
-import { Button, Grid } from "@material-ui/core";
-import constants from './constants';
-import outdent from 'outdent';
-
-function redirectToInstagram() {
-  const url = window.location.href;
-  window.location.href = outdent.string(`
-    https://api.instagram.com/oauth/authorize
-    ?client_id=${constants.appID}
-    &redirect_uri=${url.substring(0, url.lastIndexOf('/'))}${routes.instagramRedirectURI}
-    &scope=user_profile
-    &response_type=code`);
-}
-
-const InstagramLogin: React.FC = () => {
-  return (
-    <Grid
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-      style={{ minHeight: "100vh" }}
-      spacing={0}
-    >
-      <Button size="large" variant="outlined" onClick={redirectToInstagram}>
-        Login with instagram
-      </Button>
-    </Grid>
-  );
-};
+import { InstagramLogin } from "./instagramLogin";
 
 function App() {
   return (
---
+++ src/components/instagramLogin.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import routes from "./routes";
+import { Button, Grid } from "@material-ui/core";
+import constants from "./constants";
+import outdent from "outdent";
+
+
+function redirectToInstagram() {
+  const url = window.location.href;
+  window.location.href = outdent.string(`
+    https://api.instagram.com/oauth/authorize
+    ?client_id=${constants.appID}
+    &redirect_uri=${url.substring(0, url.lastIndexOf("/"))}${
+    routes.instagramRedirectURI
+  }
+    &scope=user_profile
+    &response_type=code`);
+}
+
+export const InstagramLogin: React.FC = () => {
+  return (
+    <Grid
+      container
+      direction="row"
+      justify="center"
+      alignItems="center"
+      style={{ minHeight: "100vh" }}
+      spacing={0}
+    >
+      <Button size="large" variant="outlined" onClick={redirectToInstagram}>
+        Login with instagram
+      </Button>
+    </Grid>
+  );
+};
