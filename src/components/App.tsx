0c301bc6af00492f927c9022f643af53a07fca97
Itay Levy <itaylevy134@gmail.com>
2020-03-21 08:07:51 +0200

    Able to extract the code out of the redirect

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,24 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, useLocation } from "react-router-dom";
 import React from "react";
 import routes from "./routes";
-import { InstagramLogin } from "./instagramLogin";
+import { InstagramLogin } from './instagramLogin';
+
+const InstagramRedirect: React.FC = () => {
+  const code = (new URLSearchParams(useLocation().search)).get('code');
+  console.log(code);
+  return (
+    <div>
+      TODO
+    </div>
+  )
+}
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path={routes.instagramLogin} component={InstagramLogin} />
+        <Route path={routes.instagramRedirectURI} component={InstagramRedirect}/>
       </Switch>
     </Router>
   );
